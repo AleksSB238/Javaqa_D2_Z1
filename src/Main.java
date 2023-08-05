eb0c811148e7ee6e258328047b532f5418eb056c
qa test <aleksandrbabkin238@gmail.com>
2023-08-05 18:25:04 +0500

    chander src

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Пополнение счета телефона");
         int score = 100;
-        int payment_amount = 1100;
-        System.out.println("на  " + payment_amount + " руб.");
+        int paymentAmount = 1100;
+        System.out.println("на  " + paymentAmount + " руб.");
         int bonus;
 
-        if (payment_amount > 1000){
-            bonus = payment_amount / 100;
+        if (paymentAmount > 1000){
+            bonus = paymentAmount / 100;
         } else {
             bonus = 0;
         }
 
-        int final_score = score + payment_amount + bonus;
+        int finalScore = score + paymentAmount + bonus;
 
-        System.out.println("Итого на счету :  " + final_score + " руб." + "  Из них бонусами :  " + bonus + " руб.");
+        System.out.println("Итого на счету :  " + finalScore + " руб." + "  Из них бонусами :  " + bonus + " руб.");
 
     }
 }
